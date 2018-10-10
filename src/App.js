3bbe8a87245230799b81d5765eb1a5fddfb718c1
Adam Butler <adam@lab.io>
2018-10-10 11:47:00 +0100

    Move list data up to the App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import List from './components/List';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listItems: []
+    }
+
+    this.loadToDoItems()
+  }
+
+  loadToDoItems() {
+    fetch('//127.0.0.1:3001')
+    .then((response) => response.json())
+    .then((payload) => {
+      this.setState({
+        listItems: payload,
+      })
+    })
+    .catch((e) => {
+      console.error('Could not fetch list items', e)
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Ordoo To Do!</h1>
-          <List></List>
+          <List listItems={ this.state.listItems }></List>
         </header>
       </div>
     );
--- src/components/List.js
+++ src/components/List.js
@@ -3,34 +3,8 @@ import ListItem from './ListItem'
 import '../List.css';
 
 class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listItems: [
-        { id: 1, title: 'Item 1' },
-        { id: 2, title: 'Item 2' },
-        { id: 3, title: 'Item 3' },
-      ]
-    }
-
-    this.loadToDoItems()
-  }
-
-  loadToDoItems() {
-    fetch('//127.0.0.1:3001')
-    .then((response) => response.json())
-    .then((payload) => {
-      this.setState({
-        listItems: payload,
-      })
-    })
-    .catch((e) => {
-      console.error('Could not fetch list items', e)
-    })
-  }
-
   renderListItems() {
-    return this.state.listItems.map((listItem) => {
+    return this.props.listItems.map((listItem) => {
       return (
         <ListItem item={ listItem }></ListItem>
       )
